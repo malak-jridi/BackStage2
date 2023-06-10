f9adba9dd081340c9cb4848fd582ef3285f24777
thebinsohail <anasbinsohail@outloook.com>
2023-06-11 01:29:19 +0500

    admin request dto

---
+++ src/main/java/com/StudentLoginv01/StudentLoginv01/payload/AdminDto/AdminLoginRequestDTO.java
@@ -0,0 +1,21 @@
+package com.StudentLoginv01.StudentLoginv01.payload.AdminDto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class AdminLoginRequestDTO {
+    @Email
+    @NotBlank(message = "email cannot be empty")
+    @NotNull(message = "email cannot be null")
+    private String email;
+    @NotBlank(message = "password cannot be empty")
+    @NotNull(message = "password cannot be null")
+    private String password;
+}
